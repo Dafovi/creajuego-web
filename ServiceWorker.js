07dd2eaa7ff50d87368d0c9165164d7d7735275a
Dafovi <swan364@hotmail.com>
2026-09-20 11:01:36 -0500

    compression format changed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DafoviSAS-Dafovi_LabCo-1.0";
 const contentToCache = [
-    "Build/WebGlFirtsTest.loader.js",
-    "Build/WebGlFirtsTest.framework.js.gz",
-    "Build/WebGlFirtsTest.data.gz",
-    "Build/WebGlFirtsTest.wasm.gz",
+    "Build/creajuego-web.loader.js",
+    "Build/creajuego-web.framework.js.br",
+    "Build/creajuego-web.data.br",
+    "Build/creajuego-web.wasm.br",
     "TemplateData/style.css"
 
 ];
